10650f91b1cc4852d282280564b3c93897c1c318
Pirijan <hi@pirijan.com>
2022-10-08 13:38:44 -0400

    check for missing params, stub scraper

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A python3 micro-service that retrieves twitter conversation threads
 
+Hosted on …
+// url
+
 # Install
 
     pip3 install -r requirements.txt
@@ -11,6 +14,7 @@ A python3 micro-service that retrieves twitter conversation threads
     export FLASK_DEBUG=true
     flask run
     http://127.0.0.1:5000
+    http://127.0.0.1:5000/tweets?userId=234&conversationId=234234
 
 # Update Scraper
 
--- app.py
+++ app.py
@@ -1,17 +1,60 @@
 from flask import Flask, request
-from snscrape.modules.twitter import snscrape
+import snscrape.modules.twitter
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello, World!'
+  return {
+    'message': 'kinopio-twitter is online',
+    'docs': 'https://github.com/kinopio-club/kinopio-twitter'
+  }
+
 
 @app.route('/tweets')
-def tweets():
+def run():
+  tweets = []
+  userId = request.args.get('userId', '') # TODO use userName instead
+  conversationId = request.args.get('conversationId', '')
+  print('💖',userId, conversationId)
+  if (not userId or not conversationId):
+    return "Query params missing", 400
+
+
+
+  # search for every tweet by the user, and filter for tweets with the right conversation id
+
   # https://github.com/JustAnotherArchivist/snscrape/issues/552
+  print('🍋',snscrape.modules.twitter)
+
   scraper = snscrape.modules.twitter.TwitterUserScraper('textfiles')
-  print(scraper)
-  # for tweet in scraper.get_items():
-  #   print(tweet.url)
-  return 'yolo'
+  items = scraper.get_items()
+
+  print('🍅', next(items))
+
+  for tweet in scraper.get_items():
+    # print(dir(tweet))
+    print('🌷',tweet.conversationId, tweet.content, tweet.url)
+
+# filter by convo id
+
+
+  return {
+    'tweets': tweets
+  }
+
+
+  # add error / 500 returns when scraper fails (eg userid is wrong or something)
+
+
+# http://127.0.0.1:5000/tweets?userId=234&conversationId=234234
+
+# conversationId
+# quotedTweet
+# inReplyToTweetId
+# inReplyToUser
+
+# ['__annotations__',
+# '__class__', '__dataclass_fields__', '__dataclass_params__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'cashtags',
+#  'content', 'conversationId', 'coordinates', 'date', 'hashtags', 'id', 'inReplyToTweetId', 'inReplyToUser', 'json', 'lang', 'likeCount',
+#  'media', 'mentionedUsers', 'outlinks', 'outlinksss', 'place', 'quoteCount', 'quotedTweet', 'renderedContent', 'replyCount', 'retweetCount', 'retweetedTweet', 'source', 'sourceLabel', 'sourceUrl', 'tcooutlinks', 'tcooutlinksss', 'url', 'user', 'username']
